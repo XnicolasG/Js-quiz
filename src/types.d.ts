f3dabe8305ed46254d9120497d550dd3951631e4
Nicolás García <nicolasg.ngl@gmail.com>
2026-06-18 21:22:38 -0500

    add new type for new format of questions and update data for questions

--- src/types.d.ts
+++ src/types.d.ts
@@ -7,3 +7,14 @@ export interface Question{
     userSelectedAnswer?: number;
     isCorrectUserAnswer?: boolean
 }
+
+export type QuizQuestion = {
+  id: number
+  topic: "html" | "css" | "javascript" | "react" | "typescript"
+  difficulty: "beginner" | "intermediate" | "advanced" | "ssr"
+  type: "concept" | "code" | "trick" | "best-practice"
+  question: string
+  code?: string
+  options: string[]
+  correctIndex: number
+}
